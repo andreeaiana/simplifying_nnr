97c7e1924be1d93c9b0dff3ff30da45995e1d745
andreeaiana <andreeaiana@gmail.com>
2023-02-21 12:57:45 +0100

    Changed description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    description="nrs_design_choices",
+    description="",
     author="",
     author_email="",
     url="https://github.com/andreeaiana/nrs_design_choices",  
